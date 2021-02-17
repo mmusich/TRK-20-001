03f067c21e39b3bb09cf98f018f46d10458d00f8
mmusich <marco.musich@cern.ch>
2021-02-17 18:38:28 +0100

    add maps of bad components and code-check, code-format

--- BadComponentsCollector/plugins/BadComponentsCollector.cc
+++ BadComponentsCollector/plugins/BadComponentsCollector.cc
@@ -35,6 +35,9 @@
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 #include "FWCore/Utilities/interface/InputTag.h"
 
+#include "CondCore/SiPixelPlugins/interface/Phase1PixelMaps.h"
+#include "CondCore/SiStripPlugins/interface/SiStripTkMaps.h"
+
 #include <iostream>
 #include <cstdio>
 #include <sys/time.h>
@@ -43,15 +46,10 @@
 // class declaration
 //
 
-// If the analyzer does not use TFileService, please remove
-// the template argument to the base class so the class inherits
-// from  edm::one::EDAnalyzer<>
-// This will improve performance in multithreaded jobs.
-
 class BadComponentsCollector : public edm::one::EDAnalyzer<edm::one::SharedResources> {
 public:
   explicit BadComponentsCollector(const edm::ParameterSet&);
-  ~BadComponentsCollector();
+  ~BadComponentsCollector() override;
 
   static void fillDescriptions(edm::ConfigurationDescriptions& descriptions);
 
@@ -64,22 +62,17 @@ private:
   std::string formatedOutput_;
   edm::ESGetToken<SiPixelQuality, SiPixelQualityFromDbRcd> pixQualityToken_;
   edm::ESGetToken<SiStripBadStrip, SiStripBadChannelRcd> badStripToken_;
+
+  std::unique_ptr<Phase1PixelMaps> pixelmap;
+  std::unique_ptr<SiStripTkMaps> stripmap;
+  std::unique_ptr<SiStripTkMaps> s_ghost;
 };
-
-//
-// constants, enums and typedefs
-//
-
-//
-// static data member definitions
-//
 
 //
 // constructors and destructor
 //
-BadComponentsCollector::BadComponentsCollector(const edm::ParameterSet& iConfig):
-  formatedOutput_(iConfig.getUntrackedParameter<std::string>("outputFile", ""))
-{
+BadComponentsCollector::BadComponentsCollector(const edm::ParameterSet& iConfig)
+    : formatedOutput_(iConfig.getUntrackedParameter<std::string>("outputFile", "")) {
   pixQualityToken_ = esConsumes<SiPixelQuality, SiPixelQualityFromDbRcd>();
   badStripToken_ = esConsumes<SiStripBadStrip, SiStripBadChannelRcd>();
 }
@@ -102,11 +95,17 @@ void BadComponentsCollector::analyze(const edm::Event& iEvent, const edm::EventS
   const auto& pixelQuality = &iSetup.getData(pixQualityToken_);
   auto theDisabledModules = pixelQuality->getBadComponentList();
   for (const auto& mod : theDisabledModules) {
-    if (pixelQuality->IsModuleBad(mod.DetID)) { 
+    int subid = DetId(mod.DetID).subdetId();
+    if (pixelQuality->IsModuleBad(mod.DetID)) {
+      if (subid == PixelSubdetector::PixelBarrel) {
+        pixelmap->fillBarrelBin("bcBarrel", mod.DetID, 1);
+      } else {
+        pixelmap->fillForwardBin("bcForward", mod.DetID, 1);
+      }
       edm::LogPrint("BadComponentsCollector") << mod.DetID << std::endl;
       if (pFile) {
-	fprintf(pFile, "%i ", mod.DetID);
-	fprintf(pFile, "\n");
+        fprintf(pFile, "%i ", mod.DetID);
+        fprintf(pFile, "\n");
       }
     }
   }
@@ -121,34 +120,60 @@ void BadComponentsCollector::analyze(const edm::Event& iEvent, const edm::EventS
   std::vector<uint32_t> detids;
   payload->getDetIds(detids);
   for (const auto& detid : detids) {
-    if (siStripQuality_->IsModuleBad(detid)) { 
+    if (siStripQuality_->IsModuleBad(detid)) {
+      stripmap->fill(detid, 1);
       edm::LogPrint("BadComponentsCollector") << detid << std::endl;
       if (pFile) {
-	fprintf(pFile, "%i ", detid);
-	fprintf(pFile, "\n");
+        fprintf(pFile, "%i ", detid);
+        fprintf(pFile, "\n");
       }
     }
   }
- if (pFile){
-   fclose(pFile);
- }
+  if (pFile) {
+    fclose(pFile);
+  }
 }
-
 
 // ------------ method called once each job just before starting event loop  ------------
 void BadComponentsCollector::beginJob() {
-  // please remove this method if not needed
+  pixelmap = std::make_unique<Phase1PixelMaps>("COLZ0 L");
+  pixelmap->bookBarrelHistograms("bcBarrel", "inactive modules", "inactive modules");
+  pixelmap->bookBarrelBins("bcBarrel");
+  pixelmap->bookForwardHistograms("bcForward", "inactive modules", "inactive modules");
+  pixelmap->bookForwardBins("bcForward");
+
+  std::string titleMap = "Bad components map";
+
+  stripmap = std::make_unique<SiStripTkMaps>("COLZA0 L");
+  stripmap->bookMap(titleMap, "Disabled Strip modules");
+
+  s_ghost = std::make_unique<SiStripTkMaps>("AL");
+  s_ghost->bookMap(titleMap, "");
 }
 
 // ------------ method called once each job just after ending the event loop  ------------
 void BadComponentsCollector::endJob() {
-  // please remove this method if not needed
+  gStyle->SetPalette(kRainBow);
+  pixelmap->beautifyAllHistograms();
+
+  TCanvas cB("CanvBarrel", "CanvBarrel", 1200, 1000);
+  pixelmap->DrawBarrelMaps("bcBarrel", cB);
+  cB.SaveAs("pixelBarrelBadComponents.png");
+
+  TCanvas cF("CanvForward", "CanvForward", 1600, 1000);
+  pixelmap->DrawForwardMaps("bcForward", cF);
+  cF.SaveAs("pixelForwardBadComponents.png");
+
+  TCanvas cS("CanvStrips", "Strip disabled modules");
+  stripmap->drawMap(cS, "");
+  s_ghost->drawMap(cS, "same");
+  cS.SaveAs("stripBadComponents.png");
 }
 
 // ------------ method fills 'descriptions' with the allowed parameters for the module  ------------
 void BadComponentsCollector::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
   edm::ParameterSetDescription desc;
-  desc.addUntracked<std::string>("outputFile","badComponents.txt");
+  desc.addUntracked<std::string>("outputFile", "badComponents.txt");
   descriptions.addWithDefaultLabel(desc);
 }
 
